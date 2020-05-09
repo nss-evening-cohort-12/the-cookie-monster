d821a10a3318ebf065dff62d7513e4e56a2cef58
Matt Gill <mtgill90@outlook.com>
2020-05-09 13:38:46 -0500

    additional cookies added

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const cookies = [{
-    id: '0',
+const cookies = [
+{   id: '0',
     title: 'Chocolate Chip',
     imageUrl: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-andes-chocolate-chip-cookies-053-1544221847.jpg?crop=0.668xw:1.00xh;0.113xw,0&resize=480:*',
     description: 'Cookie with chocolate chips',
@@ -9,6 +9,38 @@ const cookies = [{
     title: 'Peanut Butter',
     imageUrl: 'https://www.simplyrecipes.com/wp-content/uploads/2015/08/peanut-butter-cookies-vertical-a-1800.jpg',
     description: 'Cookie containing peanut butter',
-    category: 'nutty', // sweet, chocolate, fruit, etc.
-    }
+    category: 'Nutty', // sweet, chocolate, fruit, etc.
+    },
+{   id: '2',
+    title: 'Oatmeal Raisin',
+    imageUrl: 'https://www.aspicyperspective.com/wp-content/uploads/2017/01/the-best-oatmeal-raisin-cookies-12.jpg',
+    description: 'Raisins and oats in a cookie',
+    category: 'Healthy?', // sweet, chocolate, fruit, etc.
+    },
+{   id: '3',
+    title: 'Sugar Cookie',
+    imageUrl: 'https://bakingamoment.com/wp-content/uploads/2016/08/9958featured.jpg',
+    description: 'Sugary cookie',
+    category: 'Sugary', // sweet, chocolate, fruit, etc.
+    },
+{   id: '4',
+    title: 'Snickerdoodle',
+    imageUrl: 'https://www.jessicagavin.com/wp-content/uploads/2019/12/snickerdoodle-cookies-10-1200.jpg',
+    description: 'Like sugar cookies, but better',
+    category: 'Sugary', // sweet, chocolate, fruit, etc.
+    },
+{   id: '5',
+    title: 'Macaroon',
+    imageUrl: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F7426387.jpg&w=1200&h=1200&c=sc&poi=face&q=85',
+    description: 'Fancy cookie',
+    category: 'Sweet', // sweet, chocolate, fruit, etc.
+    },
 ];
+
+const cookieObj = () => {
+    cookies.forEach(cookie => {
+        console.log(cookie);
+    });
+};
+
+cookieObj();
