85f500a83f434f897ac041962b9fce850143e9b3
Matt Gill <mtgill90@outlook.com>
2020-05-09 12:50:19 -0500

    created array with cookie objects

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('🍪🍪🍪🍪🍪')
+const cookies = [{
+    id: '0',
+    title: 'Chocolate Chip',
+    imageUrl: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-andes-chocolate-chip-cookies-053-1544221847.jpg?crop=0.668xw:1.00xh;0.113xw,0&resize=480:*',
+    description: 'Cookie with chocolate chips',
+    category: 'Chocolate', // sweet, chocolate, fruit, etc.
+    },
+{   id: '1',
+    title: 'Peanut Butter',
+    imageUrl: 'https://www.simplyrecipes.com/wp-content/uploads/2015/08/peanut-butter-cookies-vertical-a-1800.jpg',
+    description: 'Cookie containing peanut butter',
+    category: 'nutty', // sweet, chocolate, fruit, etc.
+    }
+];
